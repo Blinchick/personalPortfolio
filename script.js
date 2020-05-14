d7a61347449023e10811593ca52059dd903ade6f
Blinchick <annaspiridonenkova@gmail.com>
2020-05-14 13:53:35 -0400

    made typed.js work

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
+// Can also be included with a regular script tag
+
+let options = {
+    strings: ['pirdo', 'pridonk', 'piridonenkova'],
+    typeSpeed: 80,
+    backSpeed: 80,
+    smartBackspace: true, // this is a default
+    // loop: true
+};
+
+let typed = new Typed('.typed', options);
+
 // humburger menu
 $(".mobileMenu").click(function () {
     $(".icon").toggleClass("close");
     $("header nav").toggleClass("open")
 });
+
